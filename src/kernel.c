684b3866b4973509ad5e10dfe880bd5ccbf56ece
Luan Roberto Nunes <LUANROBERTONUNES@HOTMAIL.COM>
2024-11-15 19:30:05 -0300

    first ver ata disk driver

--- src/kernel.c
+++ src/kernel.c
@@ -5,6 +5,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* video_mem = 0;
 
@@ -102,6 +103,9 @@ void kernel_main()
 
     // Enable paging
     enable_paging();
+
+    char buf[512];
+    disk_read_sector(0, 1, buf);
 
     //Sys interrupts
     enable_interrupts();
