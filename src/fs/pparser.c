bb22b6a6f7f532308f557b95ab9d0dda6bc6be5b
Luan Roberto Nunes <LUANROBERTONUNES@HOTMAIL.COM>
2024-11-16 19:14:38 -0300

    feat: fs start

---
+++ src/fs/pparser.c
@@ -0,0 +1,6 @@
+#include "pparser.h"
+
+static int path_valid_format(const char* filename)
+{
+    int len = strlen(filename, LAUROS_MAX_PATH)
+}
---
+++ src/string/string.h
@@ -0,0 +1,9 @@
+#ifndef STRING_H
+#define STRING_H
+#include <stdbool.h>
+
+int strlen(const char* ptr);
+bool isdigit(char c);
+int asciitonumeric(char c);
+
+#endif
